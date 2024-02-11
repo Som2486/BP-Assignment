a01bda44142175ba1407718f1ab8539cac0609fb
Som2486 <mindflair2486@gmail.com>
2024-02-11 20:54:32 +0530

    Update main.js

--- bp/.storybook/main.js
+++ bp/.storybook/main.js
@@ -1,4 +1,14 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  webpackFinal: async (config, { configType }) => {
+    // Modify the webpack config to work with Next.js
+    config.resolve.alias['@'] = path.resolve(__dirname, '../');
+    return config;
+  },
+};
+
 const config = {
   stories: [
     "../stories/**/*.mdx",
